75cc9899847f1dd2e4d8aac621ee519bc34abb43
anuD <adilbagi22@gmail.com>
2023-06-14 07:06:23 -0700

    Added list of questions and reqd packages

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter Project title',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter Project description',
+        name: 'desc'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions',
+        name: 'instructions'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines',
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions',
+        name: 'instructions'
+    },
+    {
+        type: 'list',
+        message: 'Choose a License',
+        name: 'license',
+        choices: ['MIT', 'CCO', 'GNU', 'Apache']
+    },
+    {
+        type: 'input',
+        message: 'Enter GitHub username',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Enter GitHub email',
+        name: 'email'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
